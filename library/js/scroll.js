0fa0f3841f1505f67e0affca47e8c4c386eec4e5
Aleksandr Shumatbaev <sh889@ya.ru>
2023-08-08 16:02:47 +0300

    fix: menu scroll

--- library/js/scroll.js
+++ library/js/scroll.js
@@ -2,15 +2,13 @@ const links = document.querySelectorAll('.header-nav__link');
 
 links.forEach(link => {
   link.addEventListener('click', (e) => {
-    e.preventDefault();
-
     const id = link.getAttribute('href');
     const section = document.querySelector(id);
 
     if (section) {
       seamless.scrollIntoView(section, {
         behavior: "smooth",
-        block: "center",
+        block: "start",
         inline: "center",
       });
     }
