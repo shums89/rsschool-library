573fc90f633b9e5603a2fb6e33ce706b9953ced5
Aleksandr Shumatbaev <sh889@ya.ru>
2023-08-18 22:35:54 +0500

    feat: switching tabs in Favorites

--- library/js/index.js
+++ library/js/index.js
@@ -54,3 +54,50 @@ const changeSlide = (e) => {
 
 aboutSliderControl.addEventListener('click', changeSlide);
 aboutSliderSwitches.addEventListener('click', changeSlide);
+
+//====================================================================
+
+const tab = document.querySelector('.tabs');
+const tabHeader = tab.querySelector('.tabs-header');
+
+const debounce = (callee, timeoutMs) => {
+  return function perform(...args) {
+    let previousCall = this.lastCall
+
+    this.lastCall = Date.now()
+
+    if (previousCall && this.lastCall - previousCall <= timeoutMs) {
+      clearTimeout(this.lastCallTimer)
+    }
+
+    this.lastCallTimer = setTimeout(() => callee(...args), timeoutMs)
+  }
+};
+
+const hideSeason = () => {
+  const tabsContentSeason = tab.querySelector('.tabs-content__seasons');
+  tabsContentSeason.classList.add('fade-in');
+  tabsContentSeason.classList.remove('fade-out');
+};
+
+const changeSeasons = (e) => {
+  const target = e.target;
+  const tabsContentSeason = tab.querySelector('.tabs-content__seasons');
+  const tabsContentSeasons = tab.querySelectorAll('.tabs-content__season');
+
+  tabsContentSeasons.forEach((element) => {
+    if (element.closest(`.${target.value}`)) {
+      element.classList.remove('visually-hidden');
+    } else {
+      element.classList.add('visually-hidden');
+    }
+  });
+
+  tabsContentSeason.classList.remove('fade-in');
+  tabsContentSeason.classList.add('fade-out');
+};
+
+const changeSeasonsWithDebounce = debounce(changeSeasons, 1000)
+
+tabHeader.addEventListener('change', hideSeason);
+tabHeader.addEventListener('change', changeSeasonsWithDebounce);
