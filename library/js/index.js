af589d0b2a485a1f31ced54c596afb943fc9ddcc
Aleksandr Shumatbaev <sh889@ya.ru>
2023-08-20 13:03:11 +0500

    feat: add dropdown for profile

--- library/js/index.js
+++ library/js/index.js
@@ -1,3 +1,7 @@
+//====================================================================
+// ABOUT
+//====================================================================
+
 const aboutSlider = document.querySelector('.about-slider');
 const aboutSliderList = aboutSlider.querySelector('.about-slider__list');
 const aboutSliderItems = aboutSliderList.querySelectorAll('.about-slider__item');
@@ -56,6 +60,8 @@ aboutSliderControl.addEventListener('click', changeSlide);
 aboutSliderSwitches.addEventListener('click', changeSlide);
 
 //====================================================================
+// FAVORITES
+//====================================================================
 
 const tab = document.querySelector('.tabs');
 const tabHeader = tab.querySelector('.tabs-header');
@@ -101,3 +107,67 @@ const changeSeasonsWithDebounce = debounce(changeSeasons, 1000)
 
 tabHeader.addEventListener('change', hideSeason);
 tabHeader.addEventListener('change', changeSeasonsWithDebounce);
+
+//====================================================================
+// PROFILE
+//====================================================================
+
+const headerProfile = document.querySelector('.header-profile');
+const profileDropdown = headerProfile.querySelector('.profile-dropdown');
+
+const closeDropdown = e => {
+  if (profileDropdown.classList.contains('profile-dropdown_show') && !e.target.closest('.header-profile__btn') && !e.target.closest('.profile-dropdown')) {
+    profileDropdown.classList.remove('profile-dropdown_show');
+    document.removeEventListener('click', closeDropdown);
+  }
+};
+
+const toggleDropdown = () => {
+  profileDropdown.classList.toggle('profile-dropdown_show');
+
+  if (profileDropdown.matches('.profile-dropdown_show')) {
+    const dropdownContents = profileDropdown.querySelectorAll('.profile-dropdown__content')
+    const auth = false ? 'with-auth' : 'no-auth';
+
+    dropdownContents.forEach(element => {
+      if (element.matches(`.profile-dropdown__content_${auth}`)) {
+        element.classList.add('profile-dropdown__content_show');
+      } else {
+        element.classList.remove('profile-dropdown__content_show');
+      }
+    });
+
+    document.addEventListener('click', closeDropdown);
+  }
+};
+
+const processingHeaderProfile = e => {
+  const target = e.target;
+
+  if (target.closest('.header-profile__btn')) {
+    toggleDropdown();
+    return;
+  }
+
+  if (target.closest('.profile-dropdown__login')) {
+    console.log('log in');
+    return;
+  }
+
+  if (target.closest('.profile-dropdown__register')) {
+    console.log('register');
+    return;
+  }
+
+  if (target.closest('.profile-dropdown__my-profile')) {
+    console.log('my profile');
+    return;
+  }
+
+  if (target.closest('.profile-dropdown__logout')) {
+    console.log('log out');
+    return;
+  }
+};
+
+headerProfile.addEventListener('click', processingHeaderProfile);
