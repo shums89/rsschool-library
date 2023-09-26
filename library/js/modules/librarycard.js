6c9264f3a65a4abb69efee40d507b45fa1a4ed9f
Aleksandr Shumatbaev <sh889@ya.ru>
2023-09-26 11:22:40 +0500

    feat: add Check the card functionality

--- library/js/modules/librarycard.js
+++ library/js/modules/librarycard.js
@@ -4,11 +4,48 @@ import { openModalProfile } from "./profile.js";
 
 const libraryCard = document.querySelector('.librarycard');
 const findcardTitle = libraryCard.querySelector('.findcard__title');
-const findcardProfileVisitsCount = libraryCard.querySelector('.findcard-profile__visits-count');
-const findcardProfileBooksCount = libraryCard.querySelector('.findcard-profile__books-count');
+const findcardProfile = libraryCard.querySelector('.findcard-profile');
+const findcardProfileVisitsCount = findcardProfile.querySelector('.findcard-profile__visits-count');
+const findcardProfileBooksCount = findcardProfile.querySelector('.findcard-profile__books-count');
 const visitProfileBtn = libraryCard.querySelector('.visit-profile__btn-profile');
-const findcardFormName = libraryCard.querySelector('.findcard-form__name');
-const findcardFormCard = libraryCard.querySelector('.findcard-form__card');
+const findcardForm = libraryCard.querySelector('.findcard-form');
+const findcardFormName = findcardForm.querySelector('.findcard-form__name');
+const findcardFormCard = findcardForm.querySelector('.findcard-form__card');
+const findcardFormBtn = findcardForm.querySelector('.findcard-form__btn');
+
+const openCardInfo = e => {
+  e.preventDefault();
+
+  const ownBooks = getBooksOwn();
+
+  if (!localStorage.getItem('card')
+    || !localStorage.getItem('name')
+    || !localStorage.getItem('surname')
+    || !localStorage.getItem('email')
+    || !localStorage.getItem('password')
+  ) {
+    return;
+  }
+
+  if (findcardFormName.value == `${localStorage.getItem('name')} ${localStorage.getItem('surname')}`
+    && findcardFormCard.value == localStorage.getItem('card')
+  ) {
+    findcardProfileVisitsCount.textContent = localStorage.getItem('visits') ? localStorage.getItem('visits') : 1;
+    findcardProfileBooksCount.textContent = ownBooks.length;
+    findcardProfile.style.display = 'block';
+    findcardFormBtn.style.display = 'none';
+    setTimeout(closeCardInfo, 10000);
+  } else {
+    alert('Card not found!');
+  }
+};
+
+const closeCardInfo = () => {
+  findcardFormName.value = '';
+  findcardFormCard.value = '';
+  findcardProfile.style.display = '';
+  findcardFormBtn.style.display = '';
+};
 
 export const renderLibraryCard = () => {
   const ownBooks = getBooksOwn();
@@ -23,6 +60,7 @@ export const renderLibraryCard = () => {
     findcardProfileVisitsCount.textContent = localStorage.getItem('visits') ? localStorage.getItem('visits') : 1;
     findcardProfileBooksCount.textContent = ownBooks.length;
     visitProfileBtn.addEventListener('click', openModalProfile);
+    findcardForm.removeEventListener('submit', openCardInfo);
   } else {
     libraryCard.classList.remove('librarycard_auth');
     findcardTitle.textContent = 'Find your Library card';
@@ -31,5 +69,6 @@ export const renderLibraryCard = () => {
     findcardFormCard.value = '';
     findcardFormCard.disabled = false;
     visitProfileBtn.removeEventListener('click', openModalProfile);
+    findcardForm.addEventListener('submit', openCardInfo);
   }
 };
